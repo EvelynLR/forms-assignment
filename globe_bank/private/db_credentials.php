8cc679639010be7ee2b53906d0ec06ff5d124a88
EvelynLR <evelynguan@yahoo.com>
2024-10-30 10:25:26 -0600

    commit

---
+++ globe_bank/private/db_credentials.php
@@ -0,0 +1,14 @@
+<?php
+
+define("DB_SERVER", "localhost");
+
+//localhost
+define("DB_USER", "webuser");
+define("DB_PASS", "secretpassword");
+define("DB_NAME", "globe_bank");
+
+//Host Huski
+//define("DB_USER", "evelyngu_webuser");
+// define("DB_PASS", "z,kfYER*JN42");
+// define("DB_NAME", "evelyngu_globe_bank");
+?>
